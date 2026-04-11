f479227440aa3db4de0161358891570adde3a1a1
Rodroxx-one <rodroxx@gmail.com>
2026-04-10 22:01:13 -0400

    Fix N8N_PATH for correct Subfolder routing

--- app.js
+++ app.js
@@ -6,7 +6,10 @@ const port = process.env.PORT || 8080;
 // Definimos variables de entorno obligatorias
 const envArgs = { 
     ...process.env, 
-    N8N_PORT: port.toString()
+    N8N_PORT: port.toString(),
+    N8N_PATH: '/n8n-server/',
+    VUE_APP_URL_BASE_API: '/n8n-server/',    
+    WEBHOOK_URL: 'https://darkorchid-fox-796124.hostingersite.com/n8n-server/'
 };
 
 console.log("Iniciando n8n en el puerto: " + port);
